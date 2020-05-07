a25aa46fc80292380a1f4e0247048572c8307127
Muhammad Umer Saleem <omersaleem96@hotmail.com>
2020-05-07 11:28:11 +0500

    Add project files.

---
+++ Qademli/Utility/DbInitializer.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Linq;
+
+
+namespace Qademli.Utility
+{
+    public class DbInitializer
+    {
+        public static void Initialize(ApplicationDBContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any students.
+            //if (context.User.Any())
+            //{
+            //    return;   // DB has been seeded
+            //}
+
+            //var users = new User[]
+            //{
+            //    new User{ Email="admin@xyz.com",Password="admin",Role="Admin"},
+            //    new User{ Email="adnan@xyz.com",Password="123",Role="Customer"},
+            //};
+            //foreach (User u in users)
+            //{
+            //    context.User.Add(u);
+            //}
+            //context.SaveChanges();
+        }
+    }
+}
---
+++ Qademli/Utility/JWTHandler.cs
@@ -0,0 +1,38 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Qademli
+{
+    public class JWTHandler
+    {
+        private IConfiguration _config;
+        public JWTHandler(IConfiguration config)
+        {
+            _config = config;
+        }
+        //public string GenerateJSONWebToken(User userInfo)
+        //{
+        //    var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+        //    var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        //    var claims = new[] {
+        //        new Claim(ClaimTypes.Email, userInfo.Email),
+        //        new Claim(ClaimTypes.Role, userInfo.Role),
+        //        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        //    };
+
+        //    var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+        //          _config["Jwt:Issuer"],
+        //          claims,
+        //          expires: DateTime.Now.AddMinutes(120),
+        //          signingCredentials: credentials);
+
+        //    return new JwtSecurityTokenHandler().WriteToken(token);
+        //}
+    }
+}
